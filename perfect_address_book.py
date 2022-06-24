7da65e246fb2c3381b49003aa528db411484db07
Сергей Сытник <sytnikserhii22038@gmail.com>
2022-06-24 20:47:04 +0300

    created a_d, the start

---
+++ perfect_address_book.py
@@ -0,0 +1,29 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+	pass
+
+
+class Field:
+	pass
+
+
+class Record:
+	pass
+
+
+class Name(Field):
+	pass
+
+
+class Phone(Field):
+	pass
+
+
+class Email(Field):
+	pass
+
+
+class Birthday(Field):
+	pass
